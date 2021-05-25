ae4b735984e3460ff6be167f218c15089d3e0099
sunshiqi <18338830119@163com>
2021-05-25 11:24:02 +0800

    this is second test_ add new

--- src/main/java/Demo2.java
+++ src/main/java/Demo2.java
@@ -1,5 +1,5 @@
 public class Demo2 {
     public static void main(String[] args) {
-        System.out.println("this is second test");
+        System.out.println("this is second test new ");
     }
 }
